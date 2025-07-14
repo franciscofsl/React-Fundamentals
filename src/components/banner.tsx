4af467f5dac513823e5b8c733613bb8d17d6b916
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-14 18:50:09 +0200

    Styling Components: Applying CSS Classes to Components

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -2,11 +2,11 @@ import logo from '../assets/logo.png';
 
 const Banner = () => {
   return (
-    <header>
-      <div>
-        <img src={logo} alt="Logo"></img>
+    <header className="row mb-4">
+      <div className="col-5">
+        <img src={logo} className="logo" alt="Logo"></img>
       </div>
-      <div>Providing houses all over the world</div>
+      <div className="col-7 col-5">Providing houses all over the world</div>
     </header>
   );
 };
