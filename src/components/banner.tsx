49150f2ff08048dabede8ea5df1704b6d2e8845c
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-14 18:56:08 +0200

    Styling Components: CSS Modules and Expressions

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,10 +1,11 @@
 import logo from '../assets/logo.png';
+import styles from './banner.module.css';
 
 const Banner = () => {
   return (
     <header className="row mb-4">
       <div className="col-5">
-        <img src={logo} className="logo" alt="Logo"></img>
+        <img src={logo} className={styles.logo} alt="Logo"></img>
       </div>
       <div className="col-7 col-5">Providing houses all over the world</div>
     </header>
