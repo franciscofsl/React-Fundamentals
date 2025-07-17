c49df2068a3a5a76a6e75a7b89a6ec36cff528fb
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-17 20:32:15 +0200

    Component Rendering and Side Effects: Side Effects and use effect | Effects and re-rendering

--- src/components/HouseList.tsx
+++ src/components/HouseList.tsx
@@ -1,20 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HouseRow from "@/components/HouseRow";
 import House from "@/types/House";
+import housesData from "../data/houses.json";
 
-const housesArray: House[] = [
-  new House(1, "123 Main St", "USA", 300000),
-  new House(2, "456 Elm St", "Canada", 400000),
-];
+interface HouseData {
+  id: number;
+  address: string;
+  country: string;
+  price: number;
+}
 
 const HouseList = () => {
-  const [houses, setHouses] = useState(housesArray);
-  const [counter, setCounter] = useState(housesArray.length);
+  const [houses, setHouses] = useState<House[]>([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const response = await fetch("/src/data/houses.json");
+      const housesData: HouseData[] = await response.json();
+
+      const houses = housesData.map(
+        (h: HouseData) => new House(h.id, h.address, h.country, h.price)
+      );
+
+      setHouses(houses);
+      setCounter(houses.length);
+    };
+
+    fetchHouses();
+  }, []); // Empty array: the effect is executed only once when mounting the component, avoiding repeated API calls.
 
   const AddHouse = () => {
-    setHouses(
-        [...houses, 
-        House.Random(houses.length + 1)]);
+    setHouses([...houses, House.Random(houses.length + 1)]);
     // current => current + 1, ensures that the most current value is used
     setCounter((current) => current + 1);
   };
