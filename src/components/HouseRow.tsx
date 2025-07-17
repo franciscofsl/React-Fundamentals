4c1d9c500814584da27d8630a1e8bff561cfaacc
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-17 18:50:06 +0200

    fix conflicts

--- src/components/HouseRow.tsx
+++ src/components/HouseRow.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import currencyFormatter from "../helpers/CurrencyFormatter";
 
-=======
->>>>>>> 366f86ea1e85eaa12f0721f844a2ae3c655b8bb3
 type House = {
   id: number;
   address: string;
@@ -15,11 +12,7 @@ const HouseRow = ({ house }: { house: House }) => {
     <tr>
       <td>{house.address}</td>
       <td>{house.country}</td>
-<<<<<<< HEAD
       <td>{currencyFormatter.format(house.price)}</td>
-=======
-      <td>${house.price.toLocaleString()}</td>
->>>>>>> 366f86ea1e85eaa12f0721f844a2ae3c655b8bb3
     </tr>
   );
 };
